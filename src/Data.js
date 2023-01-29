86abbb5f18ea3bb1e191eabb67fa60294e9f9490
Mohit kumar <mohitkvk455@gmail.com>
2023-01-29 13:08:04 +0530

    updated

---
+++ src/Data.js
@@ -0,0 +1,55 @@
+const productData = [
+    {
+      id: 1,
+      name: "White Traditional Long Dress",
+      image:
+        "https://img.theloom.in/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spp31133_1.jpg",
+      productType: "openBackHeadphones",
+      price: 220,
+      rating: 5,
+      timeLeft: 27,
+      totalSales: 7479,
+      ctag: "UPI offer",
+      coupon:'#MOH543' 
+    },
+    {
+      id: 2,
+      name: "Long sleeve Denim jacket",
+      image:
+        "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011902396-Blue-Blue-1000011902396_01-2100.jpg",
+      productType: "openBackHeadphones",
+      price: 169,
+      rating: 5,
+      timeLeft: 24,
+      totalSales: 6898,
+      ctag: "First Order",
+      coupon:'#TYHG543'
+    },
+    {
+      id: 3,
+      name: "Modern Trendy Sneakers For Men ",
+      image:
+        "https://rukminim1.flixcart.com/image/612/612/l51d30w0/shoe/z/w/c/10-mrj1914-10-aadi-white-black-red-original-imagft9k9hydnfjp.jpeg?q=70",
+      productType: "openBackHeadphones",
+      price: 170,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 6347,
+      ctag: "Happy day",
+      coupon:'#M8546Y543'
+    },
+    {
+        id: 4,
+        name: "Modern Trendy Sneakers For Men ",
+        image:
+          "https://rukminim1.flixcart.com/image/832/832/xif0q/ethnic-set/z/h/t/xl-10056-pannkh-original-imafwfyzaybzfcpm-bb.jpeg?q=70",
+        productType: "openBackHeadphones",
+        price: 170,
+        rating: 4,
+        timeLeft: 21,
+        totalSales: 6347,
+        ctag: "super sunday",
+        coupon:'#MNH5' 
+      } 
+  ];
+  export default productData;
---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import { FaShoppingCart, FaStar } from "react-icons/fa";
+
+export function Products(props) {
+  return (
+    <div className="productList">
+      <div key={props.id} className="productCard">
+        <img src={props.image} alt="product-img" className="productImage"></img>
+        <FaShoppingCart className={"productCard__cart"} />
+
+        <div className="productCard__content">
+          <h3 className="productName">{props.name}</h3>
+          <div className="displayStack__1">
+            <div className="productPrice">₹{props.price}</div>
+            <div className="productSales">{props.totalSales} units sold</div>
+          </div>
+          <div className="displayStack__2">
+            <div className="productRating">
+              {[...Array(props.rating)].map((index) => (
+                <FaStar id={index + 1} key={index} />
+              ))}
+            </div>
+            <div className="productTime">{props.timeLeft} days left</div>
+          </div>
+        </div>
+        
+      </div>
+      <div className="coupon">
+          <div className="leftCoupon">
+            <span className="top">{props.ctag}</span><br/>
+            <span className="bottom">{props.coupon}</span>
+          </div>
+          <div className="rightCoupon">
+          <span className="top">20%</span><br/>
+          <span>OFF</span>
+          </div>
+        </div>
+    </div>
+  );
+}
